041c1b2923406a65467d8692574d929af2ca6375
Konstantin <talalaev22@.gmail.com>
2023-02-20 21:37:57 +0500

    дз сделано

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+
+    }
+
+    private static void task3() {
+        System.out.println("Задача 3");
+        String fullName = "Иванов Семён Семёнович";
+        System.out.println(fullName.replace("ё", "е"));
+    }
+
+    private static void task2() {
+        String fullName = "Ivanov Ivan Ivanovich";
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase());
+    }
+
+    private static void task1() {
+        System.out.println("Задача 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника — " + fullName);
     }
 }
